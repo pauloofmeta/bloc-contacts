6c6bf7f51a18447f702b015d0cfdc6fdc4dc7e35
Paulo Henrique Sousa da Silva <pauloofmeta@gmail.com>
2023-03-11 00:15:16 -0300

    adjustment of configurations

--- test/contact_test.dart
+++ test/contact_test.dart
@@ -1,9 +1,9 @@
 import 'package:bloc_contacts/models/contact.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Contact is worked', () {
     final contact = Contact(name: 'Person', phoneNumber: '5599999999');
     expect(contact.name, allOf(isA<String>(), equals('Person'), isNotNull));
-  }, tags: 'model');
+  });
 }
